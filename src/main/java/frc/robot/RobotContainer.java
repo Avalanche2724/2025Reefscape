e05ef3b6cc20254e9378b3a09fce703ef804f3f9
Avalanche Laptop <avalanche10078@gmail.com>
2025-01-18 14:43:54 -0500

    set deadband to 0.01

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,7 +10,6 @@ import choreo.auto.AutoChooser;
 import choreo.auto.AutoFactory;
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -30,7 +29,7 @@ public class RobotContainer {
   private final SwerveRequest.FieldCentric drive =
       new SwerveRequest.FieldCentric()
           .withDeadband(MaxSpeed * 0.01)
-          .withRotationalDeadband(MaxAngularRate * 0.01) // Add a 10% deadband
+          .withRotationalDeadband(MaxAngularRate * 0.01) // Add a 1% deadband
           .withDriveRequestType(
               DriveRequestType.Velocity); // Use open-loop control for drive motors
   private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
@@ -61,7 +60,8 @@ public class RobotContainer {
   }
 
   private double deadband(double d) {
-    return MathUtil.applyDeadband(d, 0.05);
+    return d;
+    // return MathUtil.applyDeadband(d, 0.05);
   }
 
   private void configureBindings() {
