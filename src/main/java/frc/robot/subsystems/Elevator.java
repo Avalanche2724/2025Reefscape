b816f0da728cc20e2492cf5342d96f9a889a3678
ka930 <77208047+ka930@users.noreply.github.com>
2025-01-25 14:11:26 -0500

    Work on simulation more

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -3,17 +3,21 @@ package frc.robot.subsystems;
 import static edu.wpi.first.units.Units.*;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.units.measure.Mass;
+import edu.wpi.first.wpilibj.simulation.BatterySim;
 import edu.wpi.first.wpilibj.simulation.ElevatorSim;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Elevator extends SubsystemBase {
   private static final int ELEVATOR_ID = 40;
+  private static final int ELEVATOR2_ID = 41;
+
 
   private static final double GEAR_RATIO = 20.0;
   private static final Mass MASS = Kilograms.of(4.0);
@@ -25,7 +29,7 @@ public class Elevator extends SubsystemBase {
 
   private final ElevatorSim m_elevatorSim =
       new ElevatorSim(
-          DCMotor.getKrakenX60Foc(1),
+          DCMotor.getKrakenX60Foc(2),
           GEAR_RATIO,
           MASS.in(Kilograms),
           DRUM_RADIUS.in(Meters),
@@ -37,11 +41,13 @@ public class Elevator extends SubsystemBase {
           0.0);
 
   private final TalonFX motor = new TalonFX(ELEVATOR_ID);
+  private final TalonFX followerMotor = new TalonFX(ELEVATOR_ID);
 
   public Elevator() {
     var config = new TalonFXConfiguration();
     config.Slot0.kP = 1;
     config.Feedback.RotorToSensorRatio = GEAR_RATIO;
+    followerMotor.setControl(new Follower(ELEVATOR2_ID, true));
     motor.getConfigurator().apply(config);
     motor.setPosition(0);
   }
@@ -50,26 +56,28 @@ public class Elevator extends SubsystemBase {
     motor.setControl(control.withPosition(pos));
   }
 
-  public Command downCommand() {
-    return runOnce(() -> setMotorPosition(0));
-  }
-
-  public Command upCommand() {
-    return runOnce(() -> setMotorPosition(0));
-  }
-
-  public Command midCommand() {
-    return runOnce(() -> setMotorPosition(0));
-  }
-
-  public Command upmidCommand() {
-    return runOnce(() -> setMotorPosition(0));
-  }
-
-  public Command downmidCommand() {
-    return runOnce(() -> setMotorPosition(0));
+  public Command setMotorPositionCmd(double pos) {
+    return runOnce(() -> setMotorPosition(pos));
   }
 
   @Override
-  public void simulationPeriodic() {}
+  public void simulationPeriodic() {
+    var motorSim = motor.getSimState();
+    m_elevatorSim.setInput(motorSim.getMotorVoltage());
+    m_elevatorSim.update(0.02);
+
+    motorSim.setRotorVelocity(
+      Meters.of(m_elevatorSim.getVelocityMetersPerSecond()).div(
+        DRUM_RADIUS.times(2*Math.PI)
+      ).times(Rotation.one()).div(Seconds.one()).times(GEAR_RATIO)
+    );
+
+    motorSim.setRawRotorPosition(
+      Meters.of(m_elevatorSim.getPositionMeters()).div(
+        DRUM_RADIUS.times(2*Math.PI)
+      ).times(Rotation.one()).times(GEAR_RATIO)
+    );
+
+    BatterySim.calculateDefaultBatteryLoadedVoltage(null);
+  }
 }
