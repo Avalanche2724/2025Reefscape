2ac6171eb0bef2b3066875b204234abe2d58a349
ka930 <77208047+ka930@users.noreply.github.com>
2025-03-15 15:47:40 -0400

    change before the match

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -16,7 +16,7 @@ public class Climber extends SubsystemBase {
     TalonFXConfiguration config = new TalonFXConfiguration();
     config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
 
-    config.CurrentLimits.StatorCurrentLimit = 240;
+    config.CurrentLimits.StatorCurrentLimit = 170;
     config.CurrentLimits.SupplyCurrentLimit = 90;
 
     config.CurrentLimits.StatorCurrentLimitEnable = true;
