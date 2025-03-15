cbdb31fa0277f24a3336eb36a5a15d8a9701b9b6
ka930 <77208047+ka930@users.noreply.github.com>
2025-03-15 14:18:44 -0400

    change before the match

--- src/main/java/frc/robot/Controls.java
+++ src/main/java/frc/robot/Controls.java
@@ -164,14 +164,12 @@ public class Controls {
     operator.leftJoystickPushed.whileTrue(
         superstructure.incrementWrist(() -> -1 * operator.getLeftY()));
 
-    operator
-        .rightJoystickPushed
-        .negate()
-        .whileTrue(
-            superstructure.incrementElevator(
-                () -> -0.01 * MathUtil.applyDeadband(operator.getRightY(), 0.25)));
+    operator.rightJoystickPushed.whileTrue(
+        superstructure.incrementElevator(
+            () -> -0.01 * MathUtil.applyDeadband(operator.getRightY(), 0.25)));
 
-    operator.rightJoystickPushed.whileTrue(climber.runVoltage(() -> -12 * operator.getRightX()));
+    operator.rightJoystickPushed.whileFalse(
+        climber.runVoltage(() -> -12 * MathUtil.applyDeadband(operator.getRightX(), 0.25)));
 
     operator.leftMiddle // left squares
         .onTrue(runOnce(() -> isOnCoralBindings = false));
