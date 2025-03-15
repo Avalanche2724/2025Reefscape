06b9a8b2072d32c232fe376fcd6fa33efd5120ad
ka930 <77208047+ka930@users.noreply.github.com>
2025-03-15 18:08:13 -0400

    Slow down elevator EVEN MORE

--- src/main/java/frc/robot/subsystems/superstructure/Elevator.java
+++ src/main/java/frc/robot/subsystems/superstructure/Elevator.java
@@ -110,9 +110,9 @@ public class Elevator {
     config.TorqueCurrent.PeakForwardTorqueCurrent = 20;
     config.TorqueCurrent.PeakReverseTorqueCurrent = -20;
     // Motion magic parameters
-    config.MotionMagic.MotionMagicAcceleration = 1.3; // meters per second squared
-    config.MotionMagic.MotionMagicCruiseVelocity = 1.3; // meters per second
-    config.MotionMagic.MotionMagicJerk = 13; // meters per second cubed
+    config.MotionMagic.MotionMagicAcceleration = 1.0; // meters per second squared
+    config.MotionMagic.MotionMagicCruiseVelocity = 1.0; // meters per second
+    config.MotionMagic.MotionMagicJerk = 10; // meters per second cubed
 
     // Other things
     config.CurrentLimits.StatorCurrentLimit = 40;
