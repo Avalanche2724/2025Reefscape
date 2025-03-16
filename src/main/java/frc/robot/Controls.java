ee2ddc40df30c8d24e2b4f238c73502005930ec3
ka930 <77208047+ka930@users.noreply.github.com>
2025-03-16 08:42:21 -0400

    no comment

--- src/main/java/frc/robot/Controls.java
+++ src/main/java/frc/robot/Controls.java
@@ -164,9 +164,12 @@ public class Controls {
     operator.leftJoystickPushed.whileTrue(
         superstructure.incrementWrist(() -> -1 * operator.getLeftY()));
 
-    // operator.rightJoystickPushed.whileTrue(
-    //   superstructure.incrementElevator(
-    //       () -> -0.01 * MathUtil.applyDeadband(operator.getRightY(), 0.25)));
+    operator
+        .rightJoystickPushed
+        .negate()
+        .whileTrue(
+            superstructure.incrementElevator(
+                () -> -0.01 * MathUtil.applyDeadband(operator.getRightY(), 0.25)));
 
     operator.rightJoystickPushed.whileTrue(
         climber.runVoltage(() -> -12 * MathUtil.applyDeadband(operator.getRightX(), 0.25)));
