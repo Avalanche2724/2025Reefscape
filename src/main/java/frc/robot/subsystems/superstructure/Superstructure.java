895935c4ff30ec204ea711c8c93c9eab96affbf7
ka930 <77208047+ka930@users.noreply.github.com>
2025-03-16 12:19:04 -0400

    brr

--- src/main/java/frc/robot/subsystems/superstructure/Superstructure.java
+++ src/main/java/frc/robot/subsystems/superstructure/Superstructure.java
@@ -32,8 +32,8 @@ public class Superstructure extends SubsystemBase {
     /*OUTTAKE_L2(0.927, -35),
     OUTTAKE_L3(1.3, -35),*/
     // Launching outtake:
-    OUTTAKE_L2_LAUNCH(1.025, 0),
-    OUTTAKE_L3_LAUNCH(1.46, 0),
+    OUTTAKE_L2_LAUNCH(1.00, 0),
+    OUTTAKE_L3_LAUNCH(1.435, 0),
     OUTTAKE_L4_LAUNCH(1.44, 87),
     // Vertical outtake:
     OUTTAKE_L1_VERTICAL(0.875, -45),
