f1917148b44fa320f0cef2b376c2c51e8a240cc3
ka930 <77208047+ka930@users.noreply.github.com>
2025-04-12 11:53:02 -0400

    typo

--- src/main/java/frc/robot/Controls.java
+++ src/main/java/frc/robot/Controls.java
@@ -63,7 +63,7 @@ public class Controls {
   public boolean currentlyAutoAligning = false;
   // Internal state for controls
   // this is static to make it easier to access fix later
-  publicg boolean isOnCoralBindings = true;
+  public boolean isOnCoralBindings = true;
   public Position nextTargetPosition = Position.OUTTAKE_L1;
   public Pose2d lastPoseForAutoAlign = null;
 
