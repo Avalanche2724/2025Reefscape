d5223e6f546e76f78f2955c500b4fbb036e8eae4
ka930 <77208047+ka930@users.noreply.github.com>
2025-04-15 12:06:28 -0400

    Things and update vendordeps

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,14 +4,11 @@
 
 package frc.robot;
 
-import com.ctre.phoenix6.SignalLogger;
 import edu.wpi.first.wpilibj.Threads;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import org.photonvision.estimation.OpenCVHelp;
 
 public class Robot extends TimedRobot {
   public static Robot instance;
@@ -19,32 +16,29 @@ public class Robot extends TimedRobot {
   private Command autonomousCommand;
 
   public Robot() {
-    OpenCVHelp.forceLoadOpenCV();
     instance = this;
     robotContainer = new RobotContainer();
   }
 
   @Override
-  public void robotPeriodic() {
-    // DataLogManager.start();
-    // DataLogManager.logNetworkTables(false);
+  protected void loopFunc() {
     double time = Timer.getFPGATimestamp();
+
     Threads.setCurrentThreadPriority(true, 2);
-    CommandScheduler.getInstance().run();
+    super.loopFunc();
     Threads.setCurrentThreadPriority(false, 0);
+
     double loopTime = Timer.getFPGATimestamp() - time;
-    SmartDashboard.putNumber("Loop Time ms", loopTime * 1000);
-    SignalLogger.writeDouble("loop_time", loopTime * 1000);
+    Util.logDouble("Loop Time", loopTime * 1000);
   }
 
   @Override
-  public void disabledInit() {}
+  public void robotPeriodic() {
+    CommandScheduler.getInstance().run();
+  }
 
   @Override
   public void disabledPeriodic() {}
-
-  @Override
-  public void disabledExit() {}
 
   @Override
   public void autonomousInit() {
@@ -59,9 +53,6 @@ public class Robot extends TimedRobot {
   public void autonomousPeriodic() {}
 
   @Override
-  public void autonomousExit() {}
-
-  @Override
   public void teleopInit() {
     if (autonomousCommand != null) {
       autonomousCommand.cancel();
@@ -72,18 +63,12 @@ public class Robot extends TimedRobot {
   public void teleopPeriodic() {}
 
   @Override
-  public void teleopExit() {}
-
-  @Override
   public void testInit() {
     CommandScheduler.getInstance().cancelAll();
   }
 
   @Override
   public void testPeriodic() {}
-
-  @Override
-  public void testExit() {}
 
   @Override
   public void simulationPeriodic() {}
